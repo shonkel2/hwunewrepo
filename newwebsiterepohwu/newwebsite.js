a034f592ba9bf9f4c048d92325ab5816b9ce15a0
shon.kel <ssk2011@hw.ac.uk>
2023-10-08 20:42:07 +0100

    added new files
    tried out some javascript code

---
+++ newwebsiterepohwu/newwebsite.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+function syFunction() {
+    document.getElementById("demo").innerHTML = "I am bored";
+  }
