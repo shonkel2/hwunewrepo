37debf1b8380f4ea31688d4988e3edfd60d4d93f
shon.kel <ssk2011@hw.ac.uk>
2023-10-09 00:03:53 +0100

    new commit
    made changes to my html and js

--- newwebsiterepohwu/newwebsite.js
+++ newwebsiterepohwu/newwebsite.js
@@ -4,3 +4,7 @@ function myFunction() {
 function syFunction() {
     document.getElementById("demo").innerHTML = "I am bored";
   }
+function kyFunction() {
+    document.getElementById("demo").innerHTML = "New function added.";
+  }
+    
